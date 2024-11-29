23a4157758be002894ba760618d9178a5f892522
DavidIncarbone <david.incarbone@gmail.com>
2024-11-29 13:59:04 +0100

    create and import hobbies function

--- people.js
+++ people.js
@@ -1,4 +1,16 @@
+
+// Names Function
+
+
 const person = require("./names.js");
 const firstName = "Claudio";
 const lastName = "Ferro";
 console.log(person(firstName, lastName));
+
+// Hobbies Function
+
+const myHobbiesList = require("./hobbies.js");
+const hobbyOne = "Coding";
+const hobbyTwo = "Football";
+const hobbyThree = "Music";
+console.log(myHobbiesList(hobbyOne, hobbyTwo, hobbyThree));
